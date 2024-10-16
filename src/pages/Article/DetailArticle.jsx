50fb4e441c531c055f97b3fd591f0e49b2ff664e
mdiqbalhossain5170 <mdiqbalhossain5170@gmail.com>
2024-10-16 07:35:57 +0000

    some changes in the article page

--- src/pages/Article/DetailArticle.jsx
+++ src/pages/Article/DetailArticle.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import './DetailArticle.scss';
+import { LuLink } from "react-icons/lu";
+import { Link } from "react-router-dom";
 
 
 const DetailArticle = () => {
@@ -32,11 +35,44 @@ const DetailArticle = () => {
 
 
   return (
-     <div>
+     <div className="article__detail-wrapper">
         {
             sortedArticle.map((data) => <div key={data.index}>
-                <h3>{data.title}</h3>
-                <p>{data.article}</p>
+                {/* <h3 className="article__header"><LuLink /> {data.title}</h3>
+                <p>{data.article}</p> */}
+
+                <div className='article__header'>
+                    <h1>The Title</h1>
+                    <h2 className="article__author">
+                        <Link>
+                          by Iqbal
+                        </Link>
+                    </h2>
+                </div>
+
+                <div className='article__content'>
+                   <div className='date'>
+                     <span>April</span>
+                     <span>6</span>
+                   </div>
+
+                   <div className='article'>
+                      <h2>
+                        Prolouge
+                      </h2>
+                      <h3>
+                         Posted by <Link>Iqbal</Link>
+                      </h3>
+                      <p> 
+                        <span className='first-charecter'>{data.article.slice(0, 1)}</span>
+                        {data.article.slice(1, 300)}
+                      </p>
+                      <br></br>
+                      <p>
+                      {data.article.slice(300, 700)}
+                      </p>
+                   </div>
+                </div>   
             </div>)
         }
      </div>
